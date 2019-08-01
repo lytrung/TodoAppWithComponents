c4688e65781bb45d975e809bba23bf4ee634ef52
Trung Ly <trung.ly@cit3-501m-01.acg.ac.nz>
2019-08-01 15:57:01 +1200

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import NewTodoForm from './NewTodoForm';
+import Todo from './Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      todos:[
+        {
+          id:1,
+          content: 'Ring Peter',
+          priority: 'Important'
+        },
+        {
+          id:2,
+          content: 'Water plants',
+          priority: 'Urgent'
+        },
+        {
+          id:3,
+          content: 'Get milk',
+          priority: 'Can wait'
+        }
+      ]
+    };
+  }
+
+  addTodo = (data) =>{
+    var newTodo = {
+      id:Date.now(),
+      ...data
+    };
+    var newList = [newTodo,...this.state.todos];
+    this.setState({
+      todos: newList
+    });
+  }
+  removeTodo = (id) =>{}
+  updateTodo = (id,data) =>{}
+
+  render(){
+    return (
+      <div className="wrap">
+        <div className="container">
+          <div className="todos">
+            {
+              this.state.todos.map((todo) => {
+                
+                var todoProps = {
+                  ...todo,
+                  key: todo.id
+                };
+
+                return (
+                  <Todo {...todoProps}/>
+                )
+              })
+            }
+            
+            <NewTodoForm addTodo={this.addTodo}/>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
